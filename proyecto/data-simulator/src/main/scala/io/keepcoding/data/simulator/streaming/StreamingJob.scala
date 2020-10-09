c4d69ce6ef94481b33db74bd7ae90ea115edfb04
frisinacho <frisinacho@gmail.com>
2020-10-09 20:30:00 +0200

    Parser & Show.

--- proyecto/data-simulator/src/main/scala/io/keepcoding/data/simulator/streaming/StreamingJob.scala
+++ proyecto/data-simulator/src/main/scala/io/keepcoding/data/simulator/streaming/StreamingJob.scala
@@ -1,18 +1,34 @@
 package io.keepcoding.data.simulator.streaming
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
+case class DevicesMessage(timestamp: Long, id: String, antenna_id: String, bytes: Long, app: String)
+
 trait StreamingJob {
 
   val spark: SparkSession
 
   def readFromKafka(kafkaServer: String, topic: String): DataFrame
 
+  def parserJsonData(dataFrame: DataFrame): DataFrame
+
     def run(args: Array[String]): Unit = {
       val Array(kafkaServer, topic) = args
+      println(s"Running with: ${args.toSeq}")
 
       val kafkaDF = readFromKafka(kafkaServer, topic)
+      val devicesDF = parserJsonData(kafkaDF)
 
-      println("Hello world!")
+      devicesDF
+        .writeStream
+        .format("console")
+        .start()
+        .awaitTermination()
+
+
+
+      spark.close()
+
+      // ARGS: KAFKA_SERVER:9092 devices
     }
 
 }
--- proyecto/data-simulator/src/main/scala/io/keepcoding/data/simulator/streaming/StreamingJobImpl.scala
+++ proyecto/data-simulator/src/main/scala/io/keepcoding/data/simulator/streaming/StreamingJobImpl.scala
@@ -1,4 +1,7 @@
 package io.keepcoding.data.simulator.streaming
+import org.apache.spark.sql.catalyst.ScalaReflection
+import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.types.{StringType, StructType, TimestampType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object StreamingJobImpl extends StreamingJob {
@@ -8,6 +11,8 @@ object StreamingJobImpl extends StreamingJob {
     .appName("Spark SQL KeepCoding Base")
     .getOrCreate()
 
+  import spark.implicits._
+
   override def readFromKafka(kafkaServer: String, topic: String): DataFrame = {
     spark
       .readStream
@@ -15,6 +20,13 @@ object StreamingJobImpl extends StreamingJob {
       .option("kafka.bootstrap.servers", kafkaServer)
       .option("subscribe", topic)
       .load()
+  }
+
+  override def parserJsonData(dataFrame: DataFrame): DataFrame = {
+    val schema: StructType = ScalaReflection.schemaFor[DevicesMessage].dataType.asInstanceOf[StructType]
+    dataFrame
+      .select(from_json(col("value").cast(StringType), schema).as("json"))
+      .select("json.*")
   }
 
   def main(args: Array[String]): Unit = run(args)
