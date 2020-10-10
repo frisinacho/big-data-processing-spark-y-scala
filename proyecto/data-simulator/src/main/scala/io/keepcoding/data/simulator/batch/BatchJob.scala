a7705ace5e7359309414160bf8d4827e321a0364
frisinacho <frisinacho@gmail.com>
2020-10-10 20:13:19 +0200

    Read from Parquet.

--- proyecto/data-simulator/src/main/scala/io/keepcoding/data/simulator/batch/BatchJob.scala
+++ proyecto/data-simulator/src/main/scala/io/keepcoding/data/simulator/batch/BatchJob.scala
@@ -1,6 +1,7 @@
 package io.keepcoding.data.simulator.batch
 
-import org.apache.spark.sql.SparkSession
+import java.time.OffsetDateTime
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 case class DevicesMessage(timestamp: Long, id: String, antenna_id: String, bytes: Long, app: String)
 
@@ -8,4 +9,18 @@ trait BatchJob {
 
   val spark: SparkSession
 
+  def readFromStorage(storagePath: String, filterDate: OffsetDateTime): DataFrame
+
+  def run(args: Array[String]): Unit = {
+    val Array(filterDate, storagePath) = args
+    println(s"Running with: ${args.toSeq}")
+
+    val devicesDF = readFromStorage(storagePath, OffsetDateTime.parse(filterDate))
+
+    devicesDF.show()
+
+    spark.close()
+
+    // ARGS: <filterDate> /tmp/data-simulator/
+  }
 }
--- proyecto/data-simulator/src/main/scala/io/keepcoding/data/simulator/batch/BatchJobImpl.scala
+++ proyecto/data-simulator/src/main/scala/io/keepcoding/data/simulator/batch/BatchJobImpl.scala
@@ -1,5 +1,6 @@
 package io.keepcoding.data.simulator.batch
-import org.apache.spark.sql.SparkSession
+import java.time.OffsetDateTime
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object BatchJobImpl extends BatchJob {
   override val spark: SparkSession = SparkSession
@@ -9,4 +10,19 @@ object BatchJobImpl extends BatchJob {
     .getOrCreate()
 
   import spark.implicits._
+
+  override def readFromStorage(storagePath: String, filterDate: OffsetDateTime): DataFrame = {
+    spark
+      .read
+      .format("parquet")
+      .load(s"${storagePath}/data")
+      .filter(
+        $"year" === filterDate.getYear &&
+          $"month" === filterDate.getMonthValue &&
+          $"day" === filterDate.getDayOfMonth &&
+          $"hour" === filterDate.getHour
+      )
+  }
+
+  def main(args: Array[String]): Unit = run(args)
 }
