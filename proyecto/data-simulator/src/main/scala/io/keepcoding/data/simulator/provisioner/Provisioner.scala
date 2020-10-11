3c34c147577dfd87f9f21a615c9e36deb34b9ca9
frisinacho <frisinacho@gmail.com>
2020-10-11 12:52:32 +0200

    Translate sentence and remove unused database.

--- proyecto/data-simulator/src/main/scala/io/keepcoding/data/simulator/provisioner/Provisioner.scala
+++ proyecto/data-simulator/src/main/scala/io/keepcoding/data/simulator/provisioner/Provisioner.scala
@@ -22,13 +22,10 @@ object Provisioner {
 
       // create the statement, and run the select query
       val statement = connection.createStatement()
-      println("Conexión establecida correctamente!")
+      println("Connection established successfully!")
 
       println("Creating the user_metadata table (id TEXT, name TEXT, email TEXT, quota BIGINT).")
       statement.execute("CREATE TABLE IF NOT EXISTS user_metadata(id TEXT, name TEXT, email TEXT, quota BIGINT)")
-
-      println("Creating the bytes table (timestamp TIMESTAMP, id TEXT, value BIGINT, type TEXT).")
-      statement.execute("CREATE TABLE IF NOT EXISTS bytes(timestamp TIMESTAMP, id TEXT, value BIGINT, type TEXT)")
 
       println("Initial load for data.")
       statement.execute("INSERT INTO user_metadata (id, name, email, quota) VALUES ('00000000-0000-0000-0000-000000000001', 'andres', 'andres@gmail.com', 200000)")
